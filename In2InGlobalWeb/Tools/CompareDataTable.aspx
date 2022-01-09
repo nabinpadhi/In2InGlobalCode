be11d2d9cf0d7cd359f017df12f510cb566d4012
nabinpadhi <nabinpadhi@gmail.com>
2022-01-09 14:30:52 +0530

    Compare Datatable added

---
+++ In2InGlobalWeb/Tools/CompareDataTable.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CompareDataTable.aspx.cs" Inherits="In2InGlobal.presentation.admin.CompareDataTable" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="<%= String.Format("{0}dt={1}",ResolveUrl("css/gridview.css?"), DateTime.Now.Ticks) %>" rel="stylesheet" type="text/css" />
+    <link href="<%= String.Format("{0}dt={1}",ResolveUrl("css/style.css?"), DateTime.Now.Ticks) %>" rel="stylesheet" type="text/css" />
+    <link rel='stylesheet' type='text/css' href='Styles/StaticHeader.css' />
+  
+     <script src="../NewJEasyUI/jquery.min.js" type="text/javascript" lang="javascript"></script>
+    <script type='text/javascript' src='Styles/x.js'></script>
+
+    <script type='text/javascript' src='Styles/xtableheaderfixed.js'></script>
+
+    <script type='text/javascript'>
+        xAddEventListener(window, 'load',
+            function() { new xTableHeaderFixed('gvTheGrid', 'table-container', 0); }, false);
+    </script>
+    <style type="text/css">
+        .PageNavLink{
+            
+            vertical-align:middle;    
+            
+        }
+        .PageNavLink:link { COLOR: black; TEXT-DECORATION: none; font-weight: normal }
+        .PageNavLink:visited { COLOR: black; TEXT-DECORATION: none; font-weight: normal }
+        .PageNavLink:active { COLOR: black; TEXT-DECORATION: none }
+        .PageNavLink:hover { COLOR: yellow; TEXT-DECORATION: none; font-weight: none }
+    </style>
+</head>
+<body>
+    <center>
+        <form id="form1" runat="server">
+            <asp:ScriptManager ID="Templatescriptmanager" runat="server">
+            </asp:ScriptManager>           
+            <asp:UpdatePanel ID="pdnlTemplate" runat="server">
+                <ContentTemplate>
+                    <table>
+                        <tr>
+                            <td>
+                                OLD data
+                                <div id='oldTable-container'>
+                                    <asp:GridView ID="grdOldData" runat="server" GridLines="Both" CellPadding="3" AutoGenerateColumns="true"
+                                        BackColor="WhiteSmoke" HeaderStyle-CssClass="specify" AlternatingRowStyle-BackColor="Silver" HeaderStyle-Font-Size="Medium"
+                                        CssClass="gvTheGrid">
+                                    </asp:GridView>
+                                </div>
+                            </td>
+                            <td>
+                                Difference Data
+                                 <div id='diffTable-container'>
+                                    <asp:GridView ID="grdDiffData" runat="server" GridLines="Both" CellPadding="3" AutoGenerateColumns="true"
+                                        BackColor="WhiteSmoke" HeaderStyle-CssClass="specify" AlternatingRowStyle-BackColor="Silver" HeaderStyle-Font-Size="Medium"
+                                        CssClass="gvTheGrid">
+                                    </asp:GridView>
+                                </div>
+                            </td>
+                            <td>
+                                New Data
+                                <div id='newTable-container'>
+                                    <asp:GridView ID="grdNewData" runat="server" GridLines="Both" CellPadding="3" AutoGenerateColumns="true"
+                                        BackColor="WhiteSmoke" HeaderStyle-CssClass="specify" AlternatingRowStyle-BackColor="Silver" HeaderStyle-Font-Size="Medium"
+                                        CssClass="gvTheGrid">
+                                    </asp:GridView>
+                                </div>
+                            </td>
+                        </tr>                     
+                    </table>                   
+                               
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </form>
+    </center>
+   
+    <style type="text/css">
+         .specify {
+       
+        word-break: keep-all;       
+        white-space: nowrap;
+    }
+
+    </style>
+</body>
+</html>
---
+++ In2InGlobalWeb/Tools/CompareDataTable.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Text.RegularExpressions;
+using In2InGlobal.presentation.Tools;
+using System.Web.Services;
+using Newtonsoft.Json;
+
+namespace In2InGlobal.presentation.admin
+{
+    public partial class CompareDataTable : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+           if(!IsPostBack)
+            {
+                LoadData();
+              
+            }
+
+            
+        }
+
+        private void LoadData()
+        {
+            
+            try
+            {
+                DataTable _olddataTable=GetOldData();
+                DataTable _newDatatable=GetNewData();
+                grdOldData.DataSource = _olddataTable;
+                grdOldData.DataBind();
+                grdNewData.DataSource = _newDatatable;
+                grdNewData.DataBind();
+                grdDiffData.DataSource = CompareDatatable(_newDatatable, _olddataTable);
+                grdDiffData.DataBind();
+
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+               
+            }          
+        }
+
+       
+        private DataTable GetNewData()
+        {
+            DataTable table2 = new DataTable("articletable");
+            table2.Columns.Add("articleID");
+            table2.Columns.Add("title");
+            table2.Columns.Add("content");
+
+            for (int i = 1; i < 13; i++)
+            {
+                DataRow row = table2.NewRow();
+                row[0] = i;
+                if (i == 4)
+                {
+                    row[1] = "article name " + i.ToString() +" modified";
+                }
+                else
+                {
+                    row[1] = "article name" + i.ToString();
+                }
+                row[2] = "article written by nabin" + i.ToString();
+                table2.Rows.Add(row);
+            }
+            return table2;
+        }
+
+        private DataTable GetOldData()
+        {
+            DataTable table2 = new DataTable("articletable");
+            table2.Columns.Add("articleID");
+            table2.Columns.Add("title");
+            table2.Columns.Add("content");
+
+            for (int i = 1; i < 11; i++)
+            {
+                DataRow row = table2.NewRow();
+                row[0] = i;
+                row[1] = "article name"+i.ToString();
+                row[2] = "article written by nabin"+i.ToString();
+                table2.Rows.Add(row);
+            }
+            return table2;
+        }
+
+        private DataTable CompareDatatable(DataTable oldTable,DataTable newTable)
+        {
+            oldTable.TableName = "oldTable";
+            newTable.TableName = "newTable";
+
+            //Create Empty Table
+            DataTable diffTable = new DataTable("Difference");
+
+            try
+            {
+                //Must use a Dataset to make use of a DataRelation object
+                using (DataSet ds = new DataSet())
+                {
+                    //Add tables
+                    ds.Tables.AddRange(new DataTable[] { oldTable.Copy(), newTable.Copy() });
+
+                    //Get Columns for DataRelation
+                    DataColumn[] firstcolumns = new DataColumn[ds.Tables[0].Columns.Count];
+
+                    for (int i = 0; i < firstcolumns.Length; i++)
+                    {
+                        firstcolumns[i] = ds.Tables[0].Columns[i];
+                    }
+
+                    DataColumn[] secondcolumns = new DataColumn[ds.Tables[1].Columns.Count];
+
+                    for (int i = 0; i < secondcolumns.Length; i++)
+                    {
+                        secondcolumns[i] = ds.Tables[1].Columns[i];
+                    }
+
+                    //Create DataRelation
+                    DataRelation r = new DataRelation(string.Empty, firstcolumns, secondcolumns, false);
+
+                    ds.Relations.Add(r);
+
+                    //Create columns for return table
+                    for (int i = 0; i < oldTable.Columns.Count; i++)
+                    {
+                        diffTable.Columns.Add(oldTable.Columns[i].ColumnName, oldTable.Columns[i].DataType);
+                    }
+
+                    //If First Row not in Second, Add to return table.
+                    diffTable.BeginLoadData();
+
+                    foreach (DataRow parentrow in ds.Tables[0].Rows)
+                    {
+                        DataRow[] childrows = parentrow.GetChildRows(r);
+                        if (childrows == null || childrows.Length == 0)
+                        {
+                            diffTable.LoadDataRow(parentrow.ItemArray, true);
+                        }                       
+                    }
+
+                    diffTable.EndLoadData();
+
+                }
+            }
+            finally
+            {
+
+            }
+
+            return diffTable;
+
+        }
+    }
+}
